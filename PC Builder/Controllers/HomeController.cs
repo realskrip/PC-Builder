1d739d8d76edf176ac0ec58aa93806363ba2e4fd
Skripnikov Danila <19eazy@gmail.com>
2023-03-10 13:57:20 +0700

    0.6.1 В Index добавлены условные конструкции.

--- PC Builder/Controllers/HomeController.cs
+++ PC Builder/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace PC_Builder.Controllers
         {
             List<CPU_Manufacturer> cpu_manufacturers = db.CPU_Manufacturers.ToList();
             List<CPU> cpus = db.CPUs.ToList();
+
             List<CoolingType> coolingtypes = db.CoolingTypes.ToList();
             List<Cooling> coolings = db.Coolings.ToList();
 
@@ -26,12 +27,19 @@ namespace PC_Builder.Controllers
             { 
                 CPU_Manufacturers = cpu_manufacturers, 
                 CPUs = cpus,
+
                 CoolingTypes = coolingtypes,
                 Coolings = coolings
             };
             
+
             cpu_manufacturers.Insert(0, new CPU_Manufacturer(0, "Не выбрано"));
             coolingtypes.Insert(0, new CoolingType(0, "Не выбрано"));
+
+
+            ViewBag.cpuViewBag = CPU_Manufacturer_Id;
+            ViewBag.coolingViewBag = CoolingTypes_Id;
+
 
             if (CPU_Manufacturer_Id == 0)
             {
@@ -50,6 +58,7 @@ namespace PC_Builder.Controllers
             {
                 viewModel.Coolings = db.Coolings.Where(c => c.Id_CoolingType == CoolingTypes_Id);
             }
+
 
             return View(viewModel);
         }
