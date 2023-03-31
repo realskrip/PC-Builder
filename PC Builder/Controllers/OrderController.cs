ce21305db24ffe411f36bf2148b6c2af8f99aeb6
Skripnikov Danila <19eazy@gmail.com>
2023-03-31 18:27:50 +0700

    0.19.0 Добавлена страница оплаты заказа. Нужно добавить на нее форму.

---
+++ PC Builder/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PC_Builder.Models;
+using PC_Builder.ViewModels;
+
+namespace PC_Builder.Controllers
+{
+    public class OrderController : Controller
+    {
+        ApplicationContext db;
+        public OrderController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
