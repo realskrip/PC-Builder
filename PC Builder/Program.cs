bb27f673add9072f1c34e59bbab7ebb455c81772
Skripnikov Danila <19eazy@gmail.com>
2023-04-29 14:51:15 +0700

    0.26.1 Added support for authentication and authorization using cookies.

--- PC Builder/Program.cs
+++ PC Builder/Program.cs
@@ -1,7 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 using PC_Builder.Models;    // пространство имен класса ApplicationContext
 
 var builder = WebApplication.CreateBuilder(args);
+
+// аутентификация с помощью куки
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options => options.LoginPath = "/login");
+builder.Services.AddAuthorization();
 
 // получаем строку подключения из файла конфигурации
 string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -12,6 +21,9 @@ builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServe
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+app.UseAuthentication();   // добавление middleware аутентификации 
+app.UseAuthorization();   // добавление middleware авторизации 
 
 app.MapDefaultControllerRoute();
 app.UseStaticFiles(); // поддержка обслуживания статических файлов, например css
