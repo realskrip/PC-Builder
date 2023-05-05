3d8f967cb1db194d8453d2cca3dc4d0902ba5fbf
Skripnikov Danila <19eazy@gmail.com>
2023-05-05 21:09:05 +0700

    0.26.12 JS code moved to sortCookie.js

---
+++ PC Builder/wwwroot/js/sortCookie.js
@@ -0,0 +1,14 @@
+﻿var sortOrder = document.getElementById('sortOrder');
+sortOrder.onchange = function () {
+
+    // задаём время, через которое наш cookie истечёт
+    //var date = new Date();
+    //date.setDate(date.getDate() + 7);
+
+    // записываем cookie
+    document.cookie = 'sortOrder=' + sortOrder.value;
+
+    // вариант с временем, через которое наш cookie истечёт
+    //document.cookie = 'sortOrder=' + sortOrder.value + '; path=/; expires=' + date.toUTCString();
+
+};
